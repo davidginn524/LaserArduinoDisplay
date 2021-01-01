f18bc9f3606446060fe57ccab129db4522728262
bizit524 <bizit524@gmail.com>
2021-01-01 12:36:18 +0000

    removed unneeded code started pump/temp check

--- LaserArduinoDisplay.ino
+++ LaserArduinoDisplay.ino
@@ -14,9 +14,8 @@ U8GLIB_ST7920_128X64_1X u8g(13,11,10);	// SPI Com: SCK = en = 18, MOSI = rw = 16
 
 //Variables
 float RT, VR, ln, TX, T0, VRT;
-String mystring;
 unsigned int len;
-char buf;
+unsigned char tcheck; 
 static char temp[15];
 void draw(void) {
   // graphic commands to redraw the complete screen should be placed here  
@@ -25,29 +24,23 @@ void draw(void) {
   u8g.drawStr( 0, 10, "H2o:");   
   u8g.drawStr(30, 10, temp );
   u8g.drawStr(62, 10, "C" );
+  tempcheck();
   u8g.drawStr( 0, 21, "Pump:");
-   u8g.drawStr(62, 21, "L/H" );
+  u8g.drawStr(62, 21, "L/H" );
+  pumpcheck();
 }
-
+void tempcheck(void){
+    //u8g.drawStr(70, 10, "test" );
+}
+void pumpcheck(void){
+  
+}
 void setup(void) {
   // flip screen, if required
   u8g.setRot180();
    T0 = 25 + 273.15;
-  // assign default color value
-  if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
-    u8g.setColorIndex(255);     // white
-  }
-  else if ( u8g.getMode() == U8G_MODE_GRAY2BIT ) {
-    u8g.setColorIndex(3);         // max intensity
-  }
-  else if ( u8g.getMode() == U8G_MODE_BW ) {
-    u8g.setColorIndex(1);         // pixel on
-  }
-  else if ( u8g.getMode() == U8G_MODE_HICOLOR ) {
-    u8g.setHiColorByRGB(255,255,255);
-  }
+
   
-  pinMode(8, OUTPUT);
 }
 
 void loop(void) {
@@ -68,15 +61,9 @@ void loop(void) {
   TX = (1 / ((ln / B) + (1 / T0))); //Temperature from thermistor
 
   TX = TX - 273.15;                 //Conversion to Celsius
- 
-  Serial.print("Temperature:");
-  Serial.print("\t");
-  Serial.print(TX);
-  Serial.print("C\t\t");
-  Serial.print(TX + 273.15);        //Conversion to Kelvin
-  Serial.print("K\t\t");
-  Serial.print((TX * 1.8) + 32);    //Conversion to Fahrenheit
-  Serial.println("F");
   dtostrf(TX, 3, 1, temp);
   delay(500);
+  tcheck = "ok";
+ 
+  
 }
